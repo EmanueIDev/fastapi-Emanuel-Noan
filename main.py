6c183150c5d7687e9f9a816d8cd1e810a49cf158
EmanuelDev <emanuel.silva06@aluno.ifce.edu.br>
2025-08-25 13:27:25 +0000

    Parte 2

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="API")
+
+
+
+@app.get("/")
+def helloWorld():
+    return {"message": "Hello World"}
+
+@app.get("/aluno")
+def get_aluno():
+    return{"insira na rota após o "/" o nome do aluno"}
+
+@app.get("/aluno/{nome-aluno}")
+def get_aluno_by_name(nome_aluno: str):
+    return("O nome do aluno é:", nome_aluno)
